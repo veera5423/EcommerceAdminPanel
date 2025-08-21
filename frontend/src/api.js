cabbbbf189c2d6e45a36a2b3bbeb77fb52b70cac
Vipparla Veeranjaneyulu <veeranjaneyuluvipparla.2109@gmail.com>
2025-08-21 18:29:42 +0530

    d

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const apiUrl = import.meta.env.VITE_API_UR || 'http://localhost:8080';
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: apiUrl,
